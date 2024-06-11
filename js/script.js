e3fc972172aa1b0d7c9dc559f488531dad005509
unknown <sumitshah43621@gmail.com>
2024-06-11 06:50:34 +0530

    v1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function($){
+    // alert('okkkk');
+
+    var x = $('ul').children();
+    console.log(x);
+    x.css("border","20px solid yellow")
+})
+
+$('div').find('a').css({'border':'30px dashed black'})
